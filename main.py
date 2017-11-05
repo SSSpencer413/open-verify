8bb6bf5281f1bef972cc78baf491d27bea527dfa
Ubiw <rflorintsev@gmail.com>
2017-11-05 11:53:30 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+import asyncio #await asyncio.sleep(seconds)
+import discord
+from discord.ext import commands
+
+"""
+[BOT NAME] : Open-Verify
+[DATA STORED IN] : JSON file for now
+[DISCORD.PY] : Rewrite
+"""
+
+invite_link = "https://bit.ly/Open-Verify"
+
+bot_prefix = "$"
+
+client = commands.Bot(description="A bot. What do you expect this description to say?!?!",command_prefix=bot_prefix,game=discord.Game(name=f"{bot_prefix}verify [ROBLOXNAME]"))
+
+join_message = f"Thanks for inviting me to your discord server! Here are a few things you may want to know.\n\n**Verification**\nFor users to get verified they must do {bot_prefix}verify ROBLOXNAME. Bot will guide them through the rest.\nYou can set the verification role by doing {bot_prefix}setrole ROLENAME."
+
+@client.event
+async def on_ready():
+  print(f"Loaded as {client.user.name} with the prefix {bot_prefix}")
+
+  
+@client.event
+async def on_message(message):
+    if not message.guild is None:
+        #await client.process_commands(message)
+        pass
+    else:
+        await message.author.send(f"I only work in servers! To invite me, use {invite_link}") #damn thats smart
+        return
+    if message.content.startswith(f"{client.user.mention} invite") or message.content.startswith(f"{bot_prefix}invite"):
+        await message.channel.send("Can't wait to join your discord!\n\n**LINK**\n"+invite_link)
+				
+@client.event
+async def on_guild_join(guild):
+  for channel in guild.channels:
+    try:
+      await channel.send(join_message)
+      return
+    except:
+      continue
+  
+ 
